e07c0a6df27ddfd3140bc3af88f9453d0a1d3602
davvicval <davvicval@alum.us.es>
2025-12-14 17:56:42 +0100

    feat: Test de carga de admin role

--- app/modules/auth/tests/locustfile.py
+++ app/modules/auth/tests/locustfile.py
@@ -42,7 +42,8 @@ class LoginBehavior(TaskSet):
         csrf_token = get_csrf_token(response)
 
         response = self.client.post(
-            "/login", data={"email": "user1@example.com", "password": "1234", "csrf_token": csrf_token}
+            "/login", data={"email": "user1@example.com", "password": "1234", "csrf_token": csrf_token},
+            name="POST /login (user)",
         )
         if response.status_code != 200:
             print(f"Login failed: {response.status_code}")
@@ -53,3 +54,57 @@ class AuthUser(HttpUser):
     min_wait = 5000
     max_wait = 9000
     host = get_host_for_locust_testing()
+
+
+class ChangeRoleBehavior(TaskSet):
+
+    def on_start(self):
+        self.admin_login()
+
+    def admin_login(self):
+        # 1️⃣ GET login
+        response = self.client.get("/login")
+
+        # 2️⃣ POST login
+        login_resp = self.client.post(
+            "/login",
+            data={
+                "email": "admin@example.com",
+                "password": "1234",
+            },
+            name="POST /login (admin)",
+            allow_redirects=True,
+        )
+
+        if login_resp.status_code != 200:
+            print(f"Admin login failed: {login_resp.status_code}")
+
+    @task
+    def change_user_role(self):
+        # 3️⃣ GET admin users (para CSRF)
+        users_page = self.client.get("/admin/users")
+        if users_page.status_code != 200:
+            print(f"Failed to load admin users page: {users_page.status_code}")
+            return
+
+        # 4️⃣ POST cambio de rol
+        resp = self.client.post(
+            "/admin/users/1/role",
+            data={
+                "role": "curator",
+            },
+            name="/admin/users/<id>/role",
+        )
+
+        if resp.status_code != 200:
+            print(f"Role change failed: {resp.status_code}")
+
+class AdminUser(HttpUser):
+    tasks = [ChangeRoleBehavior]
+    min_wait = 3000
+    max_wait = 6000
+    host = get_host_for_locust_testing()
+
+
+
+
